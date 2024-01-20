d23ca748ec920a7012ecf8510f5a4c21870343f8
Arnaud <arnaud.dellam@gmail.com>
2024-01-20 23:04:26 +0100

    Checkpoint color

--- Assets/MyScripts/CheckpointScript.cs
+++ Assets/MyScripts/CheckpointScript.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 
 public class CheckpointScript : NetworkBehaviour
 {
+    [SerializeField]
+    private MeshRenderer _checkpointValidated;
+    [SerializeField]
+    private Material _greenMaterial;
+    private bool _saved = false;
     private void OnTriggerEnter(Collider other)
     {
         if (IsServer)
         {
             PlayerMovement player = other.gameObject.GetComponent<PlayerMovement>();
-            if (player)
+            if (player && !_saved)
             {
                 GameManager.Instance.NewCheckpointForThisPlayer(player.NetworkObject.OwnerClientId, this.transform);
+                _checkpointValidated.SetMaterials(new List<Material> { _greenMaterial });
+                _saved = true;
             }
         }
     }
