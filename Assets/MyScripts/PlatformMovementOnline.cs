1983f3acf5981accca07d5ba3811deee7a98efd8
Arnaud <arnaud.dellam@gmail.com>
2024-01-22 00:21:56 +0100

    PlateformMovementOnline

--- Assets/MyScripts/PlatformMovementOnline.cs
+++ Assets/MyScripts/PlatformMovementOnline.cs
@@ -8,6 +8,8 @@ public class PlatformMovementOnline : NetworkBehaviour
     [SerializeField] private Transform PlatformRoot;
 
     private Transform target;
+    private Rigidbody playerRb = null;
+    private Vector3 newPosition;
     public float Speed = 1.0f;
     void Start()
     {
@@ -15,16 +17,17 @@ public class PlatformMovementOnline : NetworkBehaviour
     }
     void Update()
     {
-        if (!IsServer) return;
         MoveTowardsTarget();
+        MoveTowardsPlayer();
     }
     private void MoveTowardsTarget()
     {
         if (target != null)
         {
             // Calcule la nouvelle position
-            Vector3 newPosition = Vector3.MoveTowards(PlatformRoot.position, target.position, Speed * Time.deltaTime);
+            newPosition = Vector3.MoveTowards(PlatformRoot.position, target.position, Speed * Time.deltaTime);
             // Déplace le GameObject vers la nouvelle position
+            if(IsServer)
             PlatformRoot.position = newPosition;
         }
 
@@ -34,5 +37,46 @@ public class PlatformMovementOnline : NetworkBehaviour
             // Alterne la cible
             target = target == EndPointA ? EndPointB : EndPointA;
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if(other.gameObject.GetComponent<NetworkObject>().OwnerClientId == NetworkManager.Singleton.LocalClientId)
+            {
+                playerRb = other.gameObject.GetComponent<Rigidbody>();
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (other.gameObject.GetComponent<NetworkObject>().OwnerClientId == NetworkManager.Singleton.LocalClientId)
+            {
+                playerRb = null;
+            }
+        }
+    }
+    private void MoveTowardsPlayer()
+    {
+        if (!playerRb) return;
+
+        Vector3 playerMove = Vector3.MoveTowards(playerRb.position, target.position, Speed * Time.deltaTime);
+
+        if (newPosition.normalized.magnitude == Vector3.right.magnitude)
+        {
+            playerMove = new Vector3(playerMove.x, playerRb.position.y, playerRb.position.z);
+        }
+        else if (newPosition.normalized.magnitude == Vector3.forward.magnitude)
+        {
+            playerMove = new Vector3(playerRb.position.x, playerRb.position.y, playerMove.z);
+        }
+        else
+        {
+            playerMove = new Vector3(playerRb.position.x, playerMove.y, playerRb.position.z);
+        }
+
+        playerRb.MovePosition(playerMove);
     }
 }
