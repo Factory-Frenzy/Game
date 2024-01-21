ae47078723fd9c20ef6f3a4a3696cf5bb685805f
Arnaud <arnaud.dellam@gmail.com>
2024-01-21 01:17:07 +0100

    PlatformMovementOnline.cs

---
+++ Assets/MyScripts/PlatformMovementOnline.cs
@@ -0,0 +1,38 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlatformMovementOnline : NetworkBehaviour
+{
+    [SerializeField] private Transform EndPointA;
+    [SerializeField] private Transform EndPointB;
+    [SerializeField] private Transform PlatformRoot;
+
+    private Transform target;
+    public float Speed = 1.0f;
+    void Start()
+    {
+        target = EndPointA;
+    }
+    void Update()
+    {
+        if (!IsServer) return;
+        MoveTowardsTarget();
+    }
+    private void MoveTowardsTarget()
+    {
+        if (target != null)
+        {
+            // Calcule la nouvelle position
+            Vector3 newPosition = Vector3.MoveTowards(PlatformRoot.position, target.position, Speed * Time.deltaTime);
+            // Déplace le GameObject vers la nouvelle position
+            PlatformRoot.position = newPosition;
+        }
+
+        // Vérifie si le GameObject a atteint la cible
+        if (Vector3.Distance(PlatformRoot.position, target.position) < 0.001f)
+        {
+            // Alterne la cible
+            target = target == EndPointA ? EndPointB : EndPointA;
+        }
+    }
+}
